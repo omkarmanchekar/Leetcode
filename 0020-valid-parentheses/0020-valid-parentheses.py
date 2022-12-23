24a5b56898429e66e374ec3acf4dca2ce86c4c03
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2022-12-22 23:40:34 -0800

    Time: 56 ms (51.96%), Space: 14 MB (26.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,8 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack  = []
         
+        stack  = []
         hash = {"(":")","{":"}","[":"]"}
+        
+        def validClosingBracket():
+            return hash[stack[-1]]
+        
         
         for i in s:
             if(i in hash):
@@ -11,7 +15,7 @@ class Solution:
                 if(len(stack) == 0):
                     return False
                 else:
-                    if(i == hash[stack[-1]]):
+                    if(i == validClosingBracket()):
                         stack.pop()
                     else:
                         return False
