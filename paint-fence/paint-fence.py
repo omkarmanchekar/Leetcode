3a2596f6187009bf6c641bfa74328024e520c050
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2022-12-29 20:21:14 -0800

    Time: 33 ms, Memory: 13.8 MB - LeetHub

--- paint-fence/paint-fence.py
+++ paint-fence/paint-fence.py
@@ -10,7 +10,7 @@ class Solution:
         dp[0] = k 
         dp[1] = k*k
         
-        for i in range(2,len(dp)):
+        for i in range(2,n):
             dp[i] = (k-1) * (dp[i-1] + dp[i-2])
             
     
