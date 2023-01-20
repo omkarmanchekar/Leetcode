46de49eb7b7fde4ef3f8dda220ee4bc5f4235200
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-01-20 14:58:49 -0800

    Time: 2324 ms (64.62%), Space: 313.2 MB (7.00%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,63 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        
+        if(source == destination):
+            return True
+        
+        
+        graph  = collections.defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+            
+        # print(graph)
+            
+        
+        visited = set()
+        # visited.append(source)
+        
+        # print("root visited   ",visited)
+        
+        
+        # def dfs(source,visited,graph,destination):
+        #     for v in graph[source]:
+        #         print("list of element   ",graph[source])
+        #         if(v not in visited):
+        #             if(v == destination):
+        #                 print("==============")
+        #                 return True
+        #             else:
+        #                 visited.append(v)
+        #                 print("Inside visited. ",visited)
+        #                 dfs(v,visited,graph,destination)
+        #                 return False
+        #     print("----------------")
+        #     # return False
+        
+        res = False
+        
+        def dfs(source,visited,graph,destination):
+            if(source == destination):
+                return True
+            
+            if(source not in visited):
+                visited.add(source)
+                for v in graph[source]:
+                    # print("list of element   ",graph[source])
+                    # if(v not in visited):
+                    #     if(v == destination):
+                    #         print("==============")
+                    #         return True
+                    #     else:
+                    #         visited.append(v)
+                    #         print("Inside visited. ",visited)
+                    res = dfs(v,visited,graph,destination)
+                    if res:
+                        return True 
+
+        
+        return dfs(source,visited,graph,destination)
+                
+            
+        
+        
