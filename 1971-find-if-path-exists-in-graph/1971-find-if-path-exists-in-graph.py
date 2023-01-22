5171ba013ca2a6a10a61bf5ea5db6e66c7935359
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-01-21 16:20:26 -0800

    Time: 2209 ms (70.02%), Space: 104.8 MB (77.69%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,32 +1,58 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        
-        # if(source == destination):
-        #     return True
-        
-        graph  = collections.defaultdict(list)
-        for u,v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
             
-        visited = [0] * (n)
-        
-        res = False
-        
-        def dfs(source,visited,graph,destination):
-            if(source == destination):
-                return True
+        # DFS approach 
+        # graph  = collections.defaultdict(list)
+#         for u,v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
             
-            if(visited[source] == 0):
-                visited[source] = 1
-                for v in graph[source]:
-                    res = dfs(v,visited,graph,destination)
-                    if res:
-                        return True 
+#         visited = [0] * (n)
+        
+#         res = False
+        
+#         def dfs(source,visited,graph,destination):
+#             if(source == destination):
+#                 return True
+            
+#             if(visited[source] == 0):
+#                 visited[source] = 1
+#                 for v in graph[source]:
+#                     res = dfs(v,visited,graph,destination)
+#                     if res:
+#                         return True 
 
         
-        return dfs(source,visited,graph,destination)
+#         return dfs(source,visited,graph,destination)
                 
+        # BFS approach 
+        
+        graph = collections.defaultdict(list)
+        for s,d in edges:
+            graph[s].append(d)
+            graph[d].append(s)
             
+        queue = []
+        queue.append(source)
+        
+        visited = [False] * n
+
+        while(queue):
+            top = queue.pop()
+            if(top == destination):
+                return True 
+            visited[top] = True
+            
+            for i in graph[top]:
+                if(visited[i] == False):
+                    visited[i] = True 
+                    queue.append(i)
+                    
+            
+        return False
+            
+            
+            
+        
         
         
