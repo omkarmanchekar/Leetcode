9530151026b7e5005f2a16733e85bf4784853fb8
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-01-25 13:41:12 -0800

    Time: 385 ms (17.13%), Space: 14.1 MB (47.15%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,53 @@
+class Solution:
+    
+#     BRUTE FORCE APPROACH 
+#     def checkUnique(self,s):
+#         counter= collections.defaultdict(int)
+#         for i in s:
+#             if(i in counter):
+#                 return False
+#             counter[i] += 1
+        
+#         return True 
+    
+    
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         res = 0
+#         for i in range(len(s)):
+#             for j in range(i,len(s)):
+#                 if(self.checkUnique(s[i:j+1])):
+#                     res = max(res,len(s[i:j+1]))
+                    
+#         return res
+                
+        
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if(len(s) == 0):
+            return 0
+        
+        unique = [0] * 128
+    
+        left = 0
+        right = 1
+        
+        unique[ord(s[0])] = 1
+        res = 1
+        while(right < len(s)):
+            if(unique[ord(s[right])] == 0):
+                res = max(res,len(s[left:right+1]))
+                unique[ord(s[right])] = 1
+                print("1",s[left:right+1])
+                right +=1 
+            else:
+                # while(left != right):
+                unique[ord(s[left])] = 0
+                left += 1
+                print("2",s[left:right+1])
+            
+            
+        return res
+            
+            
+            
+            
+                
