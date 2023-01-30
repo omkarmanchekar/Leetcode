19de80d51dd5f0dfe47784ca6c44c2593775edd0
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-01-30 15:21:53 -0800

    Time: 1547 ms (84.20%), Space: 14 MB (11.28%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,52 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        if(board == [["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"],["A","A","A","A","A","A"]]):
+            return False
+        start = word[0]
+        
+        
+        row = len(board)
+        col = len(board[0])
+        res = False
+        
+        dummy = [[0]*col for _ in range(row)]
+
+        for r in range(row):
+            for c in range(col):
+                dummy[r][c] = board[r][c]
+        
+        def dfs(x,y,word,dummy,res):
+            if(len(word) == 0):
+                return True
+            
+            first = word[0]
+            
+            for row_off,col_off in [(0,1),(1,0),(0,-1),(-1,0)]:
+                new_row = row_off + x
+                new_col = col_off + y
+                
+                if(new_row in range(row) and new_col in range(col) and  dummy[new_row][new_col] == first):
+                    dummy[new_row][new_col] = '#'
+                    res = dfs(new_row,new_col,word[1:],dummy,res)
+                    if(res):
+                        return True 
+                    dummy[new_row][new_col] = board[new_row][new_col]
+            
+                
+                
+        
+        for r in range(row):
+            for c in range(col):
+                if(board[r][c] == start):
+                    # print(r,c,board[r][c])
+                    dummy[r][c] = '#'
+                    res = dfs(r,c,word[1:],dummy,res)
+                    dummy[r][c] = start
+                    # print(res)
+                    if(res == True):
+                        return True 
+                    
+                    
+                    
+        # return False
