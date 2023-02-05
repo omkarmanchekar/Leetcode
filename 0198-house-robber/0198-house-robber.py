99a6709f135026a58d5417d910b73d02ffce5384
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-02-04 20:10:35 -0800

    Time: 43 ms (19.51%), Space: 14 MB (14.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,34 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if(len(nums) == 1):
-            return nums[0]
+#         Top-bottom (Recursive approach)
+
+        def dp(i):
+            
+            if(i == 0):
+                return nums[0]
+            if(i == 1):
+                return max(nums[0],nums[1])
+            if(i not in hash):
+                hash[i] = max(dp(i-1),dp(i-2) + nums[i])
         
-        dp = [0] * len(nums)
+            return hash[i]
         
-        dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
+        hash = {}
+        return dp(len(nums)-1)
         
-        for i in range(2,len(nums)):
-            dp[i] = max(nums[i] + dp[i-2],dp[i-1])
+        
+#         Bottom-top (iterative approach)
+
+#         if(len(nums) == 1):
+#             return nums[0]
+        
+#         dp = [0] * len(nums)
+        
+#         dp[0] = nums[0]
+#         dp[1] = max(nums[0],nums[1])
+        
+#         for i in range(2,len(nums)):
+#             dp[i] = max(nums[i] + dp[i-2],dp[i-1])
             
         
-        return dp[-1]
+#         return dp[-1]
