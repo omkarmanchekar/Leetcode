a0d16408d3a430a09913ef8fa897125c8dbacb22
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-02-09 16:16:02 -0500

    Time: 1518 ms (56.48%), Space: 29.6 MB (18.74%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,35 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+#Brute force approach
+        
+#         res = [0] * len(temperatures)
+        
+#         for i in range(len(temperatures)):
+#             for j in range(i,len(temperatures)):
+#                 if(temperatures[j]>temperatures[i]):
+#                     res[i] = (j-i)
+#                     break
+#         return res
+
+# MONOTONIC STACK approach 
+
+        res = [0] * len(temperatures)
+
+        i = 0
+        mono = []
+        while(i < len(temperatures)):
+            if(len(mono) == 0):
+                mono.append((temperatures[i],i))  
+            else:
+                if(temperatures[i] > mono[-1][0]):
+                    while(mono and mono[-1][0] < temperatures[i]):
+                        val,ind = mono.pop(-1)
+                        res[ind] = i-ind
+                mono.append((temperatures[i],i))
+            i +=1
+    
+    
+        return res
+                    
+            
