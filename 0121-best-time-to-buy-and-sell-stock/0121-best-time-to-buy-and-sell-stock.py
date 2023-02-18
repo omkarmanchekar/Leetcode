6f8e3b12fdca57c3c269be4cfa9e84d9d6fa3f6b
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-02-18 16:42:58 -0500

    Time: 1173 ms (31.48%), Space: 25.1 MB (33.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,65 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        current = float('inf')
+#         Brute Force 
+#         res = 0
+#         for i in range(len(prices)):
+#             for j in range(i,len(prices)):
+#                 if(prices[j] > prices[i]):
+#                     res = max(res,prices[j]-prices[i])
+                    
+#         return res
+    
+    
+#     find the lowest point and keep checking for max profit in single pass 
+        minn = float('inf')
+        maxx = 0
         
-        best = 0
+        for i in range(len(prices)):
+            minn = min(minn,prices[i])
+            maxx = max(maxx,prices[i] - minn)
+            
+        return maxx
         
         
-        for i in prices:
-            current = min(current,i)
-            best = max(best,i-current)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         modified kanade algorithm as kanade is mostly used when there are negative elements in input 
+        
+#         current = float('inf')
+        
+#         best = 0
+        
+        
+#         for i in prices:
+#             current = min(current,i)
+#             best = max(best,i-current)
             
             
-        return best 
+#         return best 
