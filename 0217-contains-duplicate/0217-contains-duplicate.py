278b20be5cd537893f88c2637331f38c1bdd343a
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-02-18 16:47:11 -0500

    Time: 581 ms (39.57%), Space: 31.4 MB (8.45%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
+#         Brute force O(n^2)
+#         Sorting O(nlogn)
+
+
+
+#         hash table O(n)        
         counter = collections.defaultdict(int)
         
         for i in nums:
