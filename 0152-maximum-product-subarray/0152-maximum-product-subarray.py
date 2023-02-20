2ea496fa5e583235c9ebdfce857837c0a8afb67d
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-02-19 23:45:47 -0500

    Time: 95 ms (37.91%), Space: 15.8 MB (6.24%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -6,18 +6,26 @@ class Solution:
 
 #       Similar to maximum subarray but also need to keep track of min (bcz of negative value)
 
-        minimum = maximum = nums[0]
+#         minimum = maximum = nums[0]
     
-        res = nums[0]
+#         res = nums[0]
     
-        for i in range(1,len(nums)):
+#         for i in range(1,len(nums)):
         
-            maxxx = max(nums[i],max(minimum * nums[i],maximum * nums[i]))
-            minimum = min(nums[i],min(minimum * nums[i],maximum * nums[i]))
+#             maxxx = max(nums[i],max(minimum * nums[i],maximum * nums[i]))
+#             minimum = min(nums[i],min(minimum * nums[i],maximum * nums[i]))
             
-            maximum = maxxx
+#             maximum = maxxx
             
-            res = max(maximum,res)
+#             res = max(maximum,res)
             
-        return res
+#         return res
+        
+        B = nums[::-1]
+        
+        for i in range(1,len(nums)):
+            nums[i] *= nums[i-1] or 1
+            B[i] *= B[i-1] or 1
             
+        print(nums,B,nums+B)
+        return max(nums+B)
