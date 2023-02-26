0e9aa5fe4af8b0fd3482edd839a435d214f2eb73
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-02-26 16:36:33 -0500

    Time: 49 ms (29.99%), Space: 14.1 MB (55.63%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -11,23 +11,20 @@ Traverse through whole array O(n)
 
 #  Use divide and conquer O(logn)
 
-#         def divideandconquer(num):
             
-        print(nums)
         if(len(nums) == 1):
-            # print(nums[0],type(nums[0]))
             return nums[0]
 
         if(len(nums) == 2):
-            # print(min(nums),type(min(nums)))
             return min(nums)
 
         mid = len(nums)//2
-        print(mid,nums[mid])
         
+# if the list is already sorted just return first 
         if(nums[-1] > nums[0]):
             return nums[0]
         
+# else check mofdified binary search 
         if(nums[mid] < nums[mid-1] and nums[mid] < nums[mid+1]):
             return nums[mid]
 
