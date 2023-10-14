d1781fa76e5c2eba79e8549f6b865579c80f50e4
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-10-14 18:21:13 -0400

    Time: 38 ms (68.17%), Space: 16.2 MB (59.80%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,18 +1,24 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-#         GCD has to be prefix common string in both the strings 
-#         so take the smallest one as GDC for brute force method
-
-        len1 = len(str1)
-        len2 = len(str2)
         
-        base = str1[:min(len1,len2)]
+        if(str1 + str2 != str2 + str1):
+            return ''
+    
+        s1 = len(str1)
+        s2 = len(str2)
+        
+        base = str1[:min(s1,s2)]
         
         while(base):
-            if(len1 % len(base) == 0 and len2 % len(base) == 0):
-                if((len1//len(base)) * base == str1 and (len2//len(base))* base == str2):
+            if(s1%len(base) == 0 and s2%len(base) == 0):
+                k1 = s1 // len(base)
+                k2 = s2 // len(base)
+                if(k1*base == str1 and k2* base == str2):
                     return base
                 
-            base  = base[:-1]
+            base = base[:-1]
             
         return ''
+            
+            
+            
