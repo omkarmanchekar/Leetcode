e245ac02c762f759750174ecb095f03980058d11
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-11-26 16:32:44 -0500

    Time: 246 ms (5.03%), Space: 25.3 MB (18.70%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -49,6 +49,26 @@ class Solution:
 
         return nums
 
+    
+        """
+        Can be improved/less complicated
+        left = [1] * len(nums)
+        right = [1] * len(nums)
+        
+        for i in range(1,len(nums)):
+            left[i] = left[i-1] * nums[i-1]
+            
+        for j in range(len(nums)-2,-1,-1):
+            right[j] = right[j+1] * nums[j+1]
+            
+    
+        res = [0] * len(nums)
+        
+        for i in range(len(nums)):
+            res[i] = left[i] * right[i]
+            
+        return res
+        """
 
         
 
