5f0fbbbd6dde495d3e9f8c04da5b1607051a6545
omkarmanchekar <59661624+omkarmanchekar@users.noreply.github.com>
2023-12-25 18:04:18 -0500

    Time: 540 ms (93.13%), Space: 30.4 MB (5.45%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,22 +1,40 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        '''
-        Brute Force O(N^2)
-        
-        '''
-        
+
+        """
+        basic simple first logic (Brute force) O(n^2)
+        """
+
+        # max_water = 0
+        # for left in range(len(height)-1):
+        #     for right in range(left+1,len(height)):
+        #         distance = right-left
+        #         height_container = min(height[left],height[right])
+        #         max_water = max(max_water,distance * height_container)
+
+
+        # return max_water
+
+        """
+        two pointer appaorch 
+        """
         left = 0
-        right = len(height) -1 
+        right = len(height)-1
+
+        max_water = 0
         
-        
-        res = min(height[left],height[right]) * (right-left)
-        while(left < right):
-            if(height[left] <= height[right] ):
-                left+=1
-                res = max(res,min(height[left],height[right]) * (right-left))
+        while left < right:
+            current_water = (right-left) * min(height[left],height[right]) 
+            max_water = max(max_water,current_water)
+
+            if height[left] <= height[right]:
+                left +=1
             else:
-                right -=1
-                res = max(res,min(height[left],height[right]) * (right-left))
-            
-            
-        return res
+                right-=1
+
+        return max_water
+
+        
+
+
+        
